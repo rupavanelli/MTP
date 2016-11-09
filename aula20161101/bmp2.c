485ab503073ddd8fbe4b8994d1cd3522c695f142
Ruchele Pavanelli <Ruchele Pavanelli>
2016-11-09 20:28:57 -0200

    final de aula

--- aula20161101/bmp2.c
+++ aula20161101/bmp2.c
@@ -10,7 +10,7 @@ struct Pixel
 int main()
 {
     struct Pixel pixel,pixel2;
-    int PAL,lap,BPP,WPX,i,offset;
+    int PAL,lap,BPP,WPX,i,offset,AUX,j,HPX;
     unsigned char byte;
     unsigned short word;
     unsigned int dword;
@@ -25,19 +25,25 @@ int main()
     fread(&dword,sizeof(dword),1,imagem);
     WPX=dword;
     printf("WPX: %u\n",WPX);
+    fseek(imagem,22,SEEK_SET);
+    fread(&dword,sizeof(dword),1,imagem);
+    HPX=dword;
+    printf("Altura da imagem : %u\n",HPX);
     fseek(imagem,28,SEEK_SET);
     fread(&word,sizeof(word),1,imagem);
     BPP=word;
     printf("BPP: %u\n",BPP);
     PAL = floor((BPP*WPX + 31.)/32.)*4;
     printf("tamanho da linha da array de pixels(bytes): %u\n",PAL);
-    for(i=0;i<offset;i++)
-    {
+	AUX=PAL-WPX*3;
+	for(j=0;j<HPX-1;j++)
+	{
+		for(i=0;i<WPX-1;i++)
+		{
         fread(&byte,sizeof(byte),1,imagem);
         fwrite(&byte,sizeof(byte),1,imagem2);
-    }
-    lap=0;
-    while(!feof(imagem))
+		}
+    if(AUX>0)
     {
         //leia a informação do pixel(canais R,G e B)
         if(fread(&pixel,sizeof(pixel),1,imagem))
@@ -46,16 +52,9 @@ int main()
             pixel2.G=pixel.R;
             pixel2.B=pixel.B;
             fwrite(&pixel2,sizeof(pixel2),1,imagem2);
-
-
-        }
-        lap = lap + 3;
-        if((lap + 3) > PAL)
-        {
-            fseek(imagem,(PAL - lap),SEEK_CUR);
-            lap=0;
-        }
-    }
+		}
+	}
+	}
     fclose(imagem);
     fclose(imagem2);
     return 0;
